72d8850de049bf69d58cf62c143fc89e6bcf6c57
Alexandre Garrec <garrec.alexandre@gmail.com>
2017-01-10 14:11:25 +0100

    work

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import addons from 'react-addons'
 import Hammer from 'hammerjs'
 
-
 const translate3d = (x, y) => `translate3d(${x}px, ${y}px, 0px)`
 const backgroundImage = url => `url("images/${url}")`
 
@@ -40,7 +39,7 @@ class Card extends Component {
   }
 
   render () {
-    const { initialPosition: {x, y} } = this.state
+    const { initialPosition: { x, y } } = this.state
     const initialTranslate = translate3d(x, y)
 
     var style = {
@@ -66,147 +65,123 @@ class Card extends Component {
   }
 }
 
-
-var DraggableCard = React.createClass({displayName: "DraggableCard",
-    getInitialState: function() {
-        return {
-            x: 0,
-            y: 0,
-            initialPosition: {
-                x: 0,
-                y: 0
-            },
-            startPosition: {
-                x: 0,
-                y: 0
-            },
-            animation: null
-        };
-    },
-
-    resetPosition: function() {
-        var screen = document.getElementById('master-root'),
-            card = ReactDOM.findDOMNode(this);
-
-        var initialPosition = {
-            x: Math.round((screen.offsetWidth - card.offsetWidth) / 2),
-            y: Math.round((screen.offsetHeight - card.offsetHeight) / 2)
-        };
-
-        var initialState = this.getInitialState();
-        this.setState(
-            {
-                x: initialPosition.x,
-                y: initialPosition.y,
-                initialPosition: initialPosition,
-                startPosition: initialState.startPosition
-            }
-        );
-    },
-
-    panHandlers: {
-        panstart: function() {
-            this.setState({
-                animation: false,
-                startPosition: {
-                    x: this.state.x,
-                    y: this.state.y
-                }
-            });
-        },
-        panend: function(ev) {
-            var screen = document.getElementById('master-root'),
-                card = ReactDOM.findDOMNode(this);
-
-            if (this.state.x < -50) {
-                this.props.onOutScreenLeft(this.props.cardId);
-            } else if ((this.state.x + (card.offsetWidth - 50)) > screen.offsetWidth) {
-                this.props.onOutScreenRight(this.props.cardId);
-            } else {
-                this.resetPosition();
-                this.setState({
-                    animation: true
-                });
-            }
-        },
-        panmove: function(ev) {
-            this.setState(this.calculatePosition(
-                ev.deltaX, ev.deltaY
-            ));
-        },
-        pancancel: function(ev) {
-            console.log(ev.type);
-        }
-    },
-
-    handlePan: function(ev) {
-        ev.preventDefault();
-        this.panHandlers[ev.type].call(this, ev);
-        return false;
-    },
-
-    handleSwipe: function(ev) {
-        console.log(ev.type);
-    },
-
-    calculatePosition: function(deltaX, deltaY) {
-        return {
-            x: (this.state.initialPosition.x + deltaX),
-            y: (this.state.initialPosition.y + deltaY)
-        };
-    },
-
-    componentDidMount: function() {
-        this.hammer = new Hammer.Manager(ReactDOM.findDOMNode(this));
-        this.hammer.add(new Hammer.Pan({threshold: 0}));
-
-        var events = [
-            ['panstart panend pancancel panmove', this.handlePan],
-            ['swipestart swipeend swipecancel swipemove',
-             this.handleSwipe]
-        ];
-
-        events.forEach(function(data) {
-            if (data[0] && data[1]) {
-                this.hammer.on(data[0], data[1]);
-            }
-        }, this);
-
-        this.resetPosition();
-        window.addEventListener('resize', this.resetPosition);
-    },
-
-    componentWillUnmount: function() {
-      this.hammer.stop();
-      this.hammer.destroy();
-      this.hammer = null;
-
-        window.removeEventListener('resize', this.resetPosition);
-    },
-
-    render: function() {
-        var translate = ''.concat(
-            'translate3d(',
-            this.state.x + 'px,',
-            this.state.y + 'px,',
-            '0px)'
-        );
-
-        var style = {
-            msTransform: translate,
-            WebkitTransform: translate,
-            transform: translate
-        };
-
-        var classes = {
-            animate: this.state.animation
-        };
-
-        return (React.createElement(Card, React.__spread({},  this.props, 
-                {style: style, 
-                classes: classes})));
+class DraggableCard extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      x: 0,
+      y: 0,
+      initialPosition: { x: 0, y: 0 },
+      startPosition: { x: 0, y: 0 },
+      animation: null
     }
-});
+    this.resetPosition = this.resetPosition.bind(this)
+  }
+  resetPosition () {
+    const screen = document.getElementById('master-root')
+    const card = ReactDOM.findDOMNode(this)
+
+    const initialPosition = {
+      x: Math.round((screen.offsetWidth - card.offsetWidth) / 2),
+      y: Math.round((screen.offsetHeight - card.offsetHeight) / 2)
+    }
+
+    this.setState({
+      x: initialPosition.x,
+      y: initialPosition.y,
+      initialPosition: initialPosition,
+      startPosition: { x: 0, y: 0 }
+    })
+  }
+  
+  panstart = () => {
+    this.setState({
+      animation: false,
+      startPosition: {
+        x: this.state.x,
+        y: this.state.y
+      }
+    })
+  }
+  panend = (ev) => {
+    const screen = document.getElementById('master-root')
+    const card = ReactDOM.findDOMNode(this)
+    if (this.state.x < -50) {
+      this.props.onOutScreenLeft(this.props.cardId)
+    } else if ((this.state.x + (card.offsetWidth - 50)) > screen.offsetWidth) {
+      this.props.onOutScreenRight(this.props.cardId)
+    } else {
+      this.resetPosition();
+      this.setState({ animation: true })
+    }
+  }
+  panmove = (ev) => {
+    this.setState(this.calculatePosition(
+      ev.deltaX, ev.deltaY
+    ))
+  }
+  pancancel = (ev) => {
+    console.log(ev.type);
+  }
+
+  handlePan (ev) {
+    ev.preventDefault()
+    this[ev.type](ev)
+    return false
+  }
+
+  handleSwipe (ev) {
+    console.log(ev.type)
+  }
+
+  calculatePosition (deltaX, deltaY) {
+    const { initialPosition : { x, y } } = this.state
+    return {
+      x: (x + deltaX),
+      y: (y + deltaY)
+    }
+  }
+  componentDidMount () {
+    this.hammer = new Hammer.Manager(ReactDOM.findDOMNode(this))
+    this.hammer.add(new Hammer.Pan({threshold: 2}))
+
+    const events = [
+      ['panstart panend pancancel panmove', this.handlePan],
+      ['swipestart swipeend swipecancel swipemove', this.handleSwipe]
+    ];
+
+    events.forEach((data) => {
+      if (data[0] && data[1]) {
+        this.hammer.on(data[0], (ev) => data[1].call(this, ev));
+      }
+    })
+
+    this.resetPosition()
+    window.addEventListener('resize', this.resetPosition)
+  }
+  componentWillUnmount () {
+    this.hammer.stop()
+    this.hammer.destroy()
+    this.hammer = null
+
+    window.removeEventListener('resize', this.resetPosition)
+  }
+  render () {
+    const { x, y } = this.state
+    const translate = translate3d(x, y)
+
+    const style = {
+      msTransform: translate,
+      WebkitTransform: translate,
+      transform: translate
+    }
+
+    const classes = { animate: this.state.animation }
+
+    return <Card {...this.props} style={style} classes={classes} />
+  }
+}
+
 
 class Tinderable extends Component {
   constructor (props) {
@@ -222,7 +197,7 @@ class Tinderable extends Component {
     setTimeout(() => {
       if (side === 'left') this.setState({alertLeft: false})
       else if (side === 'right') this.setState({alertRight: false})
-    }, 3000);
+    }, 300)
 
     this.setState({
       cards: this.state.cards.filter(c => c.id !== cardId),
@@ -236,8 +211,8 @@ class Tinderable extends Component {
       const props = {
         cardId: c.id,
         index: index,
-        onOutScreenLeft: this.removeCard('left'),
-        onOutScreenRight: this.removeCard('right'),
+        onOutScreenLeft: () => this.removeCard('left', c.id),
+        onOutScreenRight: () => this.removeCard('right', c.id),
         title: c.title,
         text: c.text,
         image: c.image
@@ -253,7 +228,7 @@ class Tinderable extends Component {
       'alert-left': true,
       'alert': true
     })
-    
+
     const classesAlertRight = addons.classSet({
       'alert-visible': this.state.alertRight,
       'alert-right': true,
@@ -272,4 +247,4 @@ class Tinderable extends Component {
   }
 }
 
-module.exports = Tinderable
+export default Tinderable
--- stories/index.js
+++ stories/index.js
@@ -4,9 +4,9 @@ import Cards from '../src/index'
 import './style.css'
 
 var data = [
-  {title: 'Alexandre', text: '', id: '', image: ''},
-  {title: 'Thomas', text: '', id: '', image: ''},
-  {title: 'Lucien', text: '', id: '', image: ''}
+  {title: 'Alexandre', text: '', id: 1, image: ''},
+  {title: 'Thomas', text: '', id: 2, image: ''},
+  {title: 'Lucien', text: '', id: 3, image: ''}
 ];
 
 storiesOf('Tinder card', module)
